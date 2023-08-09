92cce1650ad78b2953647d04a035de07d14bb961
lealmaxwell <maxwell.leal@hotmail.com>
2023-08-08 22:04:21 -0300

    Conexão com o MongoDB

--- src/app.js
+++ src/app.js
@@ -5,22 +5,43 @@ const app = express();
 // Configuração do .env
 require("dotenv").config();
 
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DBNAME;
+
 // Configurar middlewares e parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Conexão com o banco de dados MongoDB
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${username}:${password}@cluster0.1auplxn.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+    dbOptions
+  )
   .then(() => {
     console.log("Conexão com o MongoDB estabelecida.");
   })
   .catch((err) => {
     console.error("Erro ao conectar ao MongoDB:", err);
   });
+
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Conexão com o MongoDB estabelecida.");
+//   })
+//   .catch((err) => {
+//     console.error("Erro ao conectar ao MongoDB:", err);
+//   });
 
 // Definir as rotas
 const userRoutes = require("./routes/userRoutes");
